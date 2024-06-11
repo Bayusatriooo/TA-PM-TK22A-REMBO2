909bc6ee53478baf5807025a3344beb09c8bb54b
unknown <pratama.aja.02@gmail.com>
2024-06-11 22:57:01 +0700

    randy : menambah content list

--- program/app/src/main/java/com/uti/agrimateee/Activity/MenuActivity.kt
+++ program/app/src/main/java/com/uti/agrimateee/Activity/MenuActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.uti.agrimateee.R
 
 class MenuActivity : AppCompatActivity() {
 
