c7abcd114b5a343cf6097e9e1494cea5dde6c91d
Bayusatriooo <bayusatrio268@gmail.com>
2024-06-19 18:47:17 +0700

    master : Membuat Splashactivity kotlin

--- program/app/src/main/java/com/uti/agrimateee/Activity/SplashActivity.kt
+++ program/app/src/main/java/com/uti/agrimateee/Activity/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.uti.agrimateee.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,8 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 import com.uti.agrimateee.R
 
 class SplashActivity : AppCompatActivity() {
+    //    deklarasi variabel waktu splash screen muncul
+    private val SPLASH_TIME_OUT: Long = 3000 //3 detik
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //        membuka LoginActivity setelah splash screen selesai
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
